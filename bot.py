589ba331281cb98b83916848825a7e8ca2e892bd
Farae1 <rbastocanudo@gmail.com>
2026-06-25 20:48:21 -0300

    Configuração do bot.py, colocarei o script para rodar em um serviço de nuvem gratuito

--- bot.py
+++ bot.py
@@ -1,5 +1,97 @@
+import os
+import sqlite3
+import datetime
+import asyncio
+from zoneinfo import ZoneInfo
+from main import gerar_boletim
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+# Carrega as variáveis do arquivo .env
+load_dotenv()
 
 
+def iniciar_banco():
+    conexao = sqlite3.connect("inscritos.db")
+    cursor = conexao.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id_usuario INTEGER UNIQUE
+        )
+    ''')
+
+    conexao.commit()
+    conexao.close()
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    id_cliente = update.message.chat_id
+
+    conexao = sqlite3.connect("inscritos.db")
+    cursor = conexao.cursor()
+
+    cursor.execute("INSERT OR IGNORE INTO usuarios (id_usuario) VALUES (?)", (id_cliente,))
+
+    conexao.commit()
+    conexao.close()
+
+    await update.message.reply_text("Inscrição confirmada! Você receberá nosso boletim de notícias aqui. 🚀")
+
+
+def fatiar_mensagem(texto, limite=4000):
+    pedacos = []
+    pedaco_atual = ""
+
+    noticias = texto.split('\n\n')
+
+    for noticia in noticias:
+        if len(pedaco_atual) + len(noticia) + 2 <= limite:
+            pedaco_atual += noticia + "\n\n"
+        else:
+            if pedaco_atual:
+                pedacos.append(pedaco_atual)
+            pedaco_atual = noticia + "\n\n"
+
+    if pedaco_atual:
+        pedacos.append(pedaco_atual)
+
+    return pedacos
+
+
+async def disparar_noticias_da_noite(context: ContextTypes.DEFAULT_TYPE):
+    texto_gigante = await asyncio.to_thread(gerar_boletim)
+    lista_de_baloes = fatiar_mensagem(texto_gigante)
+
+    conexao = sqlite3.connect("inscritos.db")
+    cursor = conexao.cursor()
+
+    cursor.execute('SELECT id_usuario FROM usuarios')
+    IDs = cursor.fetchall()
+
+    for linha in IDs:
+        ID_puro = linha[0]
+
+        for balao in lista_de_baloes:
+            await context.bot.send_message(chat_id=ID_puro, text=balao, parse_mode='Markdown')
+            await asyncio.sleep(1)
+
+    conexao.close()
+if __name__ == '__main__':
+    iniciar_banco()
+
+    token_telegram = os.getenv("TOKEN_TELEGRAM")
+    app = ApplicationBuilder().token(token_telegram).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    # ---- CONFIGURAÇÃO DO RELÓGIO ----
+    horario_alvo = datetime.time(hour=20, minute=0, second=0, tzinfo=ZoneInfo("America/Sao_Paulo"))
+
+    # Agendamento Dispara todo dia às 20h:
+    app.job_queue.run_daily(disparar_noticias_da_noite, time=horario_alvo)
+
+
+    print("Bot online e aguardando...")
+    app.run_polling()
--- main.py
+++ main.py
@@ -2,43 +2,63 @@ import feedparser
 import os
 import time
 from dotenv import load_dotenv
-from newspaper import Article
+from newspaper import Article, Config
 from googlenewsdecoder import new_decoderv1
 from groq import Groq
 
-load_dotenv()
-groq_api_key = os.getenv("GROQ_API_KEY")
-client = Groq(api_key=groq_api_key)
 
-user_choose= "tecnologia"
-list_articles = []
+def gerar_boletim():
+    load_dotenv()
+    groq_api_key = os.getenv("GROQ_API_KEY")
+    client = Groq(api_key=groq_api_key)
 
-# Busca das 20 primeiras noticias no RRS Feed do Google News e extração do titulo, link e texto em uma lista
-rss_parsing = feedparser.parse("https://news.google.com/rss?hl=pt-BR&gl=BR&ceid=BR:pt-419")
-list_news = rss_parsing.entries[:20]
-for entry in list_news:
-    link = new_decoderv1(entry["link"], interval=5)
-    article = Article(link["decoded_url"])
-    article.download()
-    article.parse()
-    list_articles.append({"link" : link["decoded_url"], "title" : entry["title"],"text" : article.text})
-print(list_articles)
-# Conexão com Groq para o boletim diario de noticias
-for article in list_articles:
-    messages_package = [
-        {"role": "system", "content": """Você é um jornalista experiente, direto e objetivo. Sua missão é ler o texto bruto extraído de um site e descobrir qual é a notícia principal, resumindo-a em exatamente um parágrafo curto em português do Brasil.
-        Regras estritas:
-        Ignore completamente links soltos, propagandas, menus de navegação ou chamadas para outras matérias (como 'leia também' ou links de redes sociais).
-        Foque 100% no tema central do texto.
-        Não use saudações, introduções ou frases como 'Aqui está o resumo'. Devolva APENAS o texto do resumo pronto para ser publicado"""},
+    user_choose = "tecnologia"
+    list_articles = []
+    boletim_final = " *Boletim de Notícias*\n\n"
 
-        {"role": "user", "content": article["text"]}
-    ]
-    awnser = client.chat.completions.create(
-        messages=messages_package,
-        model="llama-3.1-8b-instant"
-    )
-    resume_awnser = awnser.choices[0].message.content
-    article["resume"] = resume_awnser
-    print(article["resume"])
-    time.sleep(5)
+    config_navegador = Config()
+    config_navegador.browser_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+
+    # Busca das 20 primeiras noticias no RRS Feed do Google News e extração do titulo, link e texto em uma lista
+    rss_parsing = feedparser.parse("https://news.google.com/rss?hl=pt-BR&gl=BR&ceid=BR:pt-419")
+    list_news = rss_parsing.entries[:20]
+
+    for entry in list_news:
+        try:
+            link = new_decoderv1(entry["link"], interval=5)
+            article = Article(link["decoded_url"], config=config_navegador)
+            article.download()
+            article.parse()
+
+            if article.text.strip():
+                list_articles.append({"link": link["decoded_url"], "title": entry["title"], "text": article.text})
+        except Exception:
+            continue
+
+    # Conexão com Groq para o boletim diario de noticias
+    for article in list_articles:
+        messages_package = [
+            {"role": "system", "content": """Você é um jornalista experiente, direto e objetivo. Sua missão é ler o texto bruto extraído de um site e descobrir qual é a notícia principal, resumindo-a em exatamente um parágrafo curto em português do Brasil.
+            Regras estritas:
+            Ignore completamente links soltos, propagandas, menus de navegação ou chamadas para outras matérias (como 'leia também' ou links de redes sociais).
+            Foque 100% no tema central do texto.
+            Não use saudações, introduções ou frases como 'Aqui está o resumo'. Devolva APENAS o texto do resumo pronto para ser publicado"""},
+
+            {"role": "user", "content": article["text"]}
+        ]
+
+        try:
+            awnser = client.chat.completions.create(
+                messages=messages_package,
+                model="llama-3.1-8b-instant"
+            )
+            resume_awnser = awnser.choices[0].message.content
+            article["resume"] = resume_awnser
+
+            boletim_final += f"🔹 *{article['title']}*\n{resume_awnser}\n🔗 {article['link']}\n\n"
+        except Exception:
+            continue
+
+        time.sleep(5)
+
+    return boletim_final
